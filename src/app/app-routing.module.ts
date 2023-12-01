41fd394dc1d579af5c6a07daedf0e74f25ae3a60
fahruanf <fachrubiannafi@gmail.com>
2023-12-01 22:00:15 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,30 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
+import { AutoLoginGuard } from './guards/auto-login.guard';
+import { AuthGuard } from './guards/auth.guard';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m =>
+      m.LoginPageModule),
+    canLoad: [AutoLoginGuard]
+  },
+  {
+    path: 'barang',
+    loadChildren: () => import('./barang/barang.module').then(m =>
+      m.BarangPageModule),
+    canLoad: [AuthGuard] // Secure all child pages
+  },
 ];
-
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
